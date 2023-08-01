e39cb9c1f71d96b0faaf68a7ab50c77e2cad6f47
Ranjan Sengupta <ranjan_201700181@smit.smu.edu.in>
2023-08-01 10:38:37 +0530

    Setting up VueJS and TailwindCSS
    Begin by setting up VueJS and TailwindCSS on your project. If you're using Laravel, Laravel Mix can help streamline this process.

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,7 +1,13 @@
-// webpack.mix.js
 const mix = require('laravel-mix');
 
 mix.js('resources/js/app.js', 'public/js')
-   .postCss('resources/css/app.css', 'public/css', [
+    .postCss('resources/css/app.scss', 'public/css', [
+        require('postcss-import'),
         require('tailwindcss'),
-    ]);
+        require('autoprefixer'),
+    ])
+    .vue();
+
+if (mix.inProduction()) {
+    mix.version();
+}
