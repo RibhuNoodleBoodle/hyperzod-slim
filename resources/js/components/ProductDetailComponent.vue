f920ec5ecf7c17e33c8493ff802cb5c811778d58
Ranjan Sengupta <ranjan_201700181@smit.smu.edu.in>
2023-08-03 07:09:12 +0530

    Update ProductDetailComponent.vue

---
+++ resources/js/components/ProductDetailComponent.vue
@@ -0,0 +1,42 @@
+<template>
+    <div class="product-detail">
+        <h2>{{ product.name }}</h2>
+        <p>{{ product.description }}</p>
+        <p>{{ product.price }}</p>
+        <button v-on:click="addToCart(product)">Add to Cart</button>
+    </div>
+</template>
+
+<script>
+import axios from 'axios';
+
+export default {
+    data() {
+        return {
+            product: {},
+        };
+    },
+    methods: {
+        getProduct() {
+            const id = this.$route.params.id;
+            axios.get(`/api/products/${id}`)
+                .then(response => {
+                    this.product = response.data;
+                })
+                .catch(error => {
+                    console.error(error);
+                });
+        },
+        addToCart(product) {
+            // Add product to cart
+        }
+    },
+    mounted() {
+        this.getProduct();
+    },
+};
+</script>
+
+<style scoped>
+/* Add your CSS styling here */
+</style>
