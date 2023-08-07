a403c04e9ec8257632ed4d915423e57e185351bb
Ranjan Sengupta <ranjan_201700181@smit.smu.edu.in>
2023-08-07 20:37:38 +0530

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -6,38 +6,20 @@ use App\Http\Controllers\CartController;
 use App\Http\Controllers\MerchantController;
 use App\Http\Controllers\OrderController;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
-
-Route::get('/', function () {
-    return view('welcome');
-});
-
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
-
 Route::middleware('auth')->group(function () {
-    Route::get('/dashboard', 'DashboardController@index')->name('dashboard');
+    Route::get('/dashboard', function () {
+        return view('dashboard');
+    })->name('dashboard');
+
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+    // Cart
+    Route::get('/cart', [CartController::class, 'show']);
+    Route::post('/cart', [CartController::class, 'store']);
+    Route::delete('/cart/{product}', [CartController::class, 'destroy']);
 });
-
-require __DIR__.'/auth.php';
-
-// Cart
-Route::get('/cart', [CartController::class, 'show']);
-Route::post('/cart', [CartController::class, 'store']);
-Route::delete('/cart/{product}', [CartController::class, 'destroy']);
 
 // Merchants
 Route::get('/merchants/nearby', [MerchantController::class, 'nearby']);
@@ -45,3 +27,8 @@ Route::get('/merchants/nearby', [MerchantController::class, 'nearby']);
 // Orders
 Route::post('/order', [OrderController::class, 'store']);
 Route::get('/order/{order}', [OrderController::class, 'show']);
+
+// Homepage
+Route::get('/', function () {
+    return view('welcome');
+});
